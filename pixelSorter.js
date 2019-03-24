a0bd02d58207f661aee2a80d6cdff0c397675cc7
m0wh <malo.dewiderspach@gmail.com>
2019-03-24 18:32:36 +0100

    :tada: Initial Commit !

---
+++ pixelSorter.js
@@ -0,0 +1,14 @@
+const imageToArray = src => {
+  // Fetch image
+  // Get pixels
+  // Return the colors as an array
+}
+
+const arrayToImage = pixelsArray => {
+  // Generates the image
+}
+
+module.exports = (src, pixelsArray) => {
+  // Pixel sorting algorithm
+  // Returns a pixels array
+}
